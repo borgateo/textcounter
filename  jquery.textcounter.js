e7541209e130ddaf416e3724d020144f2382ebd9
Matteo Borgato <borgato.matteo@gmail.com>
2013-02-27 13:02:11 +0100

    Create  jquery.textcounter.js

---
+++  jquery.textcounter.js
@@ -0,0 +1,36 @@
+;(function ($){
+    $.fn.textcounter = function(options) {
+        var that = $(this)[0];
+        var settings = {
+            sentence: "tekens beschikbaar",
+            limit: 256
+        };
+            
+        options && $.extend(settings, options);
+        
+        function setCount() {
+            var chars     = that.value.length;
+            var container = $(that).parent();
+
+            if ( chars > settings.limit ) {
+                that.value = that.value.substr(0, settings.limit);
+                chars = settings.limit;
+            }
+            var amount = settings.limit - chars;
+
+            if ( container.find(".charleft").length <= 0 ) {
+                $("<div class='charleft originalTextareaInfo'><span class='amount'>"+ amount +"</span> "+ settings.sentence +"</div>")
+                    .insertAfter(that);
+            } else {
+                container.find(".charleft span.amount").html(amount);
+            }
+        }
+
+        $(this).on("keyup focus", function() {
+            setCount();
+        });
+        setCount();
+
+    return this;
+  };
+})(jQuery);
